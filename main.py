f7bccc39fe50dff9a2b6c06a35a8a450b969d967
Arthur <arthionweb@gmail.com>
2023-05-27 14:41:24 +0100

    Fix bugs & rewrite creation of model and usage of it

--- main.py
+++ main.py
@@ -6,67 +6,67 @@ import cv2
 import numpy as np
 import pandas as pd
 from keras import Sequential
-from keras.layers import Dense, Flatten
+from keras.layers import Dense
 from keras.models import load_model
+from keras.optimizers import Adam
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
 
 
 def main():
-    generate_training_data()
+    create_model()
+    # run_model(['#ffffff', '#ffffff', '#ffffff'])
 
 
 def run_model(colors):
-    model = load_model('my_model.h5')
+    assert len(colors) == 3, "Input should be an array of 3 colors"
+
+    model = load_model('model.h5')
 
     with open('label_encoder.pkl', 'rb') as f:
         le = pickle.load(f)
 
-    colors = [hex_to_rgb(color) for color in colors]
-    colors = np.array(colors).reshape(1, 3, 3)
+    colors_rgb = [hex_to_rgb(color) for color in colors]
+    colors_flat = [component for color in colors_rgb for component in color]
 
-    prediction = model.predict(colors)
+    input_df = pd.DataFrame([colors_flat],
+                            columns=['color1_r', 'color1_g', 'color1_b', 'color2_r', 'color2_g', 'color2_b', 'color3_r',
+                                     'color3_g', 'color3_b'])
 
-    print(prediction)
+    predictions = model.predict(input_df.values)[0]
+    top_10_indices = np.argsort(predictions)[::-1][:10]
 
-    top_10 = np.argsort(prediction[0])[-10:]
-
-    for i in reversed(top_10):
-        print(le.classes_[i])
+    return le.inverse_transform(top_10_indices)
 
 
 def create_model():
     df = pd.read_csv('color_dataset.csv')
 
-    X = []
+    df['color1'] = df['color1'].apply(hex_to_rgb)
+    df['color2'] = df['color2'].apply(hex_to_rgb)
+    df['color3'] = df['color3'].apply(hex_to_rgb)
 
-    for _, row in df.iterrows():
-        rgb_colors = []
-        for i in range(1, 6):
-            rgb_colors.append(hex_to_rgb(row[f'color{i}']))
-        X.append(rgb_colors)
-
-    X = np.array(X)
-
-    y = df['class']
+    colors_df = pd.DataFrame(df['color1'].tolist(), columns=['color1_r', 'color1_g', 'color1_b'])
+    colors_df[['color2_r', 'color2_g', 'color2_b']] = pd.DataFrame(df['color2'].tolist())
+    colors_df[['color3_r', 'color3_g', 'color3_b']] = pd.DataFrame(df['color3'].tolist())
 
     le = LabelEncoder()
-    y = le.fit_transform(y)
-
-    X_train, y_train = X, y
+    classes = le.fit_transform(df['class'])
 
     model = Sequential()
-    model.add(Flatten(input_shape=(3, 3)))
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(16, input_dim=colors_df.shape[1], activation='relu'))
+    model.add(Dense(32, activation='relu'))
     model.add(Dense(len(le.classes_), activation='softmax'))
 
-    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=Adam(),
+                  metrics=['accuracy'])
 
-    model.fit(X_train, y_train, epochs=50, batch_size=16)
+    model.fit(colors_df.values, classes, epochs=50, batch_size=32)
 
-    model.save('my_model.h5')
+    model.save('model.h5')
 
+    # Save the LabelEncoder
     with open('label_encoder.pkl', 'wb') as f:
         pickle.dump(le, f)
 
@@ -109,8 +109,7 @@ def get_image_colors(img_path, n_colors):
 
 
 def hex_to_rgb(hex_color):
-    hex_color = hex_color.lstrip('#')
-    return [int(hex_color[i:i + 2], 16) for i in range(0, len(hex_color), 2)]
+    return [int(hex_color[i:i+2], 16) / 255.0 for i in (1, 3, 5)]
 
 
 if __name__ == '__main__':
